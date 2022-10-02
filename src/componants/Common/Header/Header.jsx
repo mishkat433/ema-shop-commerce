560a521f3e95e96b19cc67fa93f5906a58659d0f
miskat433 <miskat433@gmail.com>
2022-10-02 17:25:07 +0600

    navbar responsive menually

--- src/componants/Common/Header/Header.jsx
+++ src/componants/Common/Header/Header.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from "../../../images/Ema-shop-logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarChart } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+    const [bar, setBar] = useState(true)
     return (
         <div className='bg-cyan-800'>
             <div className="navbar w-11/12 mx-auto pb-4">
                 <div className="flex-1">
                     <NavLink to='/'><img className='w-44' src={logo} alt="ema-shop-logo" /></NavLink>
                 </div>
+                <FontAwesomeIcon className='text-white lg:hidden' icon={faBarChart} onClick={() => setBar(!bar)} />
                 <div className="flex-none">
-                    <ul className="flex flex-row gap-8 text-gray-200 text-lg">
-                        <li className='hover:text-white duration-300'><NavLink to='/'></NavLink></li>
-                        <li className='hover:text-white duration-300'><NavLink to='/shop'>Shop</NavLink></li>
-                        <li className='hover:text-white duration-300'><NavLink to='/'>Order</NavLink></li>
-                        <li className='hover:text-white duration-300'><NavLink to='/'>Manage Inventory</NavLink></li>
-                        <li className='hover:text-white duration-300'><NavLink to='/'>About</NavLink></li>
-                        <li className='hover:text-white duration-300'><NavLink to='/'>Login</NavLink></li>
-                    </ul>
+
+
+                    <div className={`${bar ? "block absolute top-16 text-center px-3 pb-2 right-0  bg-cyan-800" : "hidden"} lg:block `}>
+                        <ul className=" lg:flex lg:flex-row gap-8 text-gray-200 text-lg ">
+                            <li className='hover:text-white duration-300'><NavLink to='/'></NavLink></li>
+                            <li className='hover:text-white duration-300'><NavLink className={({ isActive }) => isActive ? "bg-white px-2 py-1 rounded-md  text-black" : undefined} to='/shop'>Shop</NavLink></li>
+                            <li className='hover:text-white duration-300'><NavLink to='/'>Order</NavLink></li>
+                            <li className='hover:text-white duration-300'><NavLink to='/'>Manage Inventory</NavLink></li>
+                            <li className='hover:text-white duration-300'><NavLink to='/'>About</NavLink></li>
+                            <li className='hover:text-white duration-300'><NavLink to='/'>Login</NavLink></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
-        </div>
+        </div >
     );
 };
 
--- src/componants/Home/HomeBanner/HomeBanner.jsx
+++ src/componants/Home/HomeBanner/HomeBanner.jsx
@@ -8,7 +8,7 @@ const HomeBanner = () => {
     return (
         <div className='flex flex-col-reverse lg:flex-row justify-between items-center lg:h-[80vh]'>
             <div className='lg:w-3/5'>
-                <h1 className='text-3xl lg:text-6xl font lg:leading-normal'>Start Your journey <br /> as <span className='font-semibold text-orange-600'>Ema-Shop</span></h1>
+                <h1 className='text-3xl lg:text-6xl  lg:leading-normal'>Start Your journey <br /> as <span className='font-semibold text-orange-600'>Ema-Shop</span></h1>
                 <p className=' lg:w-3/5 mt-3 text-justify'>Now a days the life style of the people is different. People feel uncomfortable and time consuming for going crowded markets. We guarantee of our product's authenticity and cover all kind of warranties.</p>
                 <Link to="/shop"><button className="btn bg-cyan-800 hover:bg-orange-600 outline-none border-none mt-5 duration-300">Shop Now
                     <FontAwesomeIcon className='ml-4 animate-ping text-xl' icon={faArrowRight} /></button>  </Link>
